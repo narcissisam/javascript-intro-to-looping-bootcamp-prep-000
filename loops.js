9bfcf3a5c353824d9377b3651972fe8200208300
Samuel Ellis <pleasecastsamellis@gmail.com>
2018-10-10 22:37:29 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,6 +1,6 @@
 function forLoop(array) {
   for (let i = 0; i < 25; i++) {
   if (i == 1) { array = ["I am 1 strange loop."];
-  } else { array.unshift "I am ${i} strange loops."]}
+  } else { array.unshift("I am ${i} strange loops.")}
 }
 }
