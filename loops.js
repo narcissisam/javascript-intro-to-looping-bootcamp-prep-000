a9618e75cd5e6131fc9bda39622715a54bc5b68a
Samuel Ellis <pleasecastsamellis@gmail.com>
2018-10-10 23:12:52 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,8 +1,9 @@
 function forLoop(array) {
   for (let i = 0; i < 25; i++) {
   if (i === 1) { array.unshift("I am 1 strange loop.");
-  } else { array.unshift("I am ${i} strange loops.");
+  } else { array.push("I am ${i} strange loops.");
   }
+  
 }
 }
 function whileLoop(n) {
